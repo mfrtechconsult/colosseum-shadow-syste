3459a3d1689ebe1be984abb6ddf10d1bb3db0c5c
M <160327578+mfrtechconsult@users.noreply.github.com>
2026-08-06 16:58:24 +0200

    Run purified summary tests on Unix

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -5,5 +5,7 @@ texlua tests/check_syntax.lua main.lua
 for file in src/part*.lua; do texlua tests/check_syntax.lua "$file"; done
 texlua tests/check_syntax.lua tests/test_shadow_math.lua
 texlua tests/check_syntax.lua tests/test_registration.lua
+texlua tests/check_syntax.lua tests/test_purified_summary.lua
 texlua tests/test_shadow_math.lua
 texlua tests/test_registration.lua
+texlua tests/test_purified_summary.lua
