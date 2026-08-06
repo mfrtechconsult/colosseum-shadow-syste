702e5a94c2aa284178faec4b7898ea769d8b174c
M <160327578+mfrtechconsult@users.noreply.github.com>
2026-08-06 16:20:52 +0200

    Check all Shadow Lua modules

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -2,6 +2,7 @@
 set -eu
 cd "$(dirname "$0")/.."
 texlua tests/check_syntax.lua main.lua
+for file in src/part*.lua; do texlua tests/check_syntax.lua "$file"; done
 texlua tests/check_syntax.lua tests/test_shadow_math.lua
 texlua tests/check_syntax.lua tests/test_registration.lua
 texlua tests/test_shadow_math.lua
