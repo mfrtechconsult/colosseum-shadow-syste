01de519bd04e4a9b0cf4af2047ef711488349f9d
M <160327578+mfrtechconsult@users.noreply.github.com>
2026-08-06 16:58:38 +0200

    Run purified summary tests on Windows

--- tests/run-tests.bat
+++ tests/run-tests.bat
@@ -4,5 +4,7 @@ texlua tests\check_syntax.lua main.lua
 for %%f in (src\part*.lua) do texlua tests\check_syntax.lua "%%f"
 texlua tests\check_syntax.lua tests\test_shadow_math.lua
 texlua tests\check_syntax.lua tests\test_registration.lua
+texlua tests\check_syntax.lua tests\test_purified_summary.lua
 texlua tests\test_shadow_math.lua
 texlua tests\test_registration.lua
+texlua tests\test_purified_summary.lua
