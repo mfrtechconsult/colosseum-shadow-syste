697d6179523bee6c20506c47f4e686eb86cbd09f
M <160327578+mfrtechconsult@users.noreply.github.com>
2026-08-06 16:21:00 +0200

    Check all Shadow Lua modules on Windows

--- tests/run-tests.bat
+++ tests/run-tests.bat
@@ -1,6 +1,7 @@
 @echo off
 cd /d "%~dp0\.."
 texlua tests\check_syntax.lua main.lua
+for %%f in (src\part*.lua) do texlua tests\check_syntax.lua "%%f"
 texlua tests\check_syntax.lua tests\test_shadow_math.lua
 texlua tests\check_syntax.lua tests\test_registration.lua
 texlua tests\test_shadow_math.lua
