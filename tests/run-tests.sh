b4c8bfe43cb818d71573aac775c9eacc0baf7dba
M <160327578+mfrtechconsult@users.noreply.github.com>
2026-08-07 09:54:24 +0200

    Run purification animation regression test

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -7,7 +7,9 @@ texlua tests/check_syntax.lua tests/test_shadow_math.lua
 texlua tests/check_syntax.lua tests/test_registration.lua
 texlua tests/check_syntax.lua tests/test_purified_summary.lua
 texlua tests/check_syntax.lua tests/test_fidelity.lua
+texlua tests/check_syntax.lua tests/test_purification_animation.lua
 texlua tests/test_shadow_math.lua
 texlua tests/test_registration.lua
 texlua tests/test_purified_summary.lua
 texlua tests/test_fidelity.lua
+texlua tests/test_purification_animation.lua
